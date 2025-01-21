d66957a0f19a9085f5d1d7c817c6a458e8a69d53
JASONGODS <gtgusion@gmail.com>
2025-01-21 08:12:20 +0700

    memperbaiki login auth

--- auth/login_auth.php
+++ auth/login_auth.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             
             ];
 
-            header('Location: ../dasboard.php');
+            header('Location: ../dashboard.php');
             exit();
 
         } else {
